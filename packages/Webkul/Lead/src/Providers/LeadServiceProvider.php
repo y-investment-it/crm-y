3b560f18dbee14a81885877e6a1faf579f6e960f
y-investment-it <y.investment.it@gmail.com>
2025-11-03 13:59:22 +0200

    build: add compiled vite assets

--- packages/Webkul/Lead/src/Providers/LeadServiceProvider.php
+++ packages/Webkul/Lead/src/Providers/LeadServiceProvider.php
@@ -15,6 +15,8 @@ class LeadServiceProvider extends ServiceProvider
     public function boot(Router $router)
     {
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+
+        $this->loadRoutesFrom(__DIR__.'/../Http/routes.php');
     }
 
     /**
